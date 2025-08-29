e42f07ed0f732fbca5b065ba9f540e7f137d8a93
Chashmishlish <salishkhannn@gmail.com>
2025-08-29 10:51:42 +0500

    Changes in form!

--- app/(root)/auth/reset-password/page.jsx
+++ app/(root)/auth/reset-password/page.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+'use client'
+import { Card, CardContent } from "@/components/ui/card";
+import React, { useState } from "react";
+import Image from "next/image";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { zSchema } from "@/lib/zodSchema";
+import { showToast } from "@/lib/showToast";
+import axios from "axios";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import ButtonLoading from "@/components/Application/ButtonLoading";
+import Link from "next/link";
+import { WEBSITE_LOGIN } from "@/routes/WebsiteRoute";
+import OTPVerification from "@/components/Application/OTPVerification";
+
 
 const resetPassword = () => {
+    const [emailVerificationLoading, setEmailVerificationLoading] = useState(false)
+    const formSchema = zSchema.pick({
+        email: true
+    })
+
+    // initializing the form
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: ""
+        }
+    })
+
+    const handleEmailVerification = async (values) => {
+
+    }
+
   return (
     <div>
-      resetPassword
+      <Card className="w-[400px]">
+      <CardContent>
+        <div className="flex justify-center">
+          <Image
+            src="/assests/images/logo-bgw.jpg"
+            width={150}
+            height={100}
+            alt="logo"
+            className="max-w-[400px]"
+          />
+        </div>
+
+        {!otpEmail ? (
+          <>
+            <div className="text-center">
+              <h1 className="text-3xl font-semibold"> Reset Password </h1>
+              <p> Enter your email to reset password. </p>
+            </div>
+            <div className="mt-5">
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleEmailVerification)}>
+                  <div className="mb-3">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="email"
+                              placeholder="example@gmail.com"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  
+                  <div className="mb-3">
+                    <ButtonLoading
+                      loading={emailVerificationLoading}
+                      type="submit"
+                      text="LoSend OTP"
+                      className="w-full cursor-pointer"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <div className="flex justify-center items-center gap-1 ">
+                      
+                      <Link
+                        href={WEBSITE_LOGIN}
+                        className="text-primary underline"
+                      >
+                        Back To Login
+                      </Link>
+                    </div>
+                    
+                  </div>
+                </form>
+              </Form>
+            </div>
+          </>
+        ) : (
+          <OTPVerification
+            email={otpEmail}
+            onSubmit={handleOtpVerification}
+            loading={otpVerificationLoading}
+            onBack={() => setOtpEmail("")} // Add back button functionality
+          />
+        )}
+      </CardContent>
+    </Card>
     </div>
   )
 }
