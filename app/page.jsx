e534f10473484de160069040848703983c1d10e7
Chashmishlish <salishkhannn@gmail.com>
2025-08-29 16:53:47 +0500

    Did a bit changes in 3000!

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,41 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import { useRouter } from "next/navigation";
+import { WEBSITE_LOGIN } from "@/routes/WebsiteRoute";
 
-const page = () => {
+const Page = () => {
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push(WEBSITE_LOGIN); // Correct: Route constant use kiya function ke andar
+  };
+
   return (
-  <div>
-    <Button>Baba and Mama</Button>
-  </div>
-  )
-}
+    <div className="h-screen w-screen flex justify-center items-center">
+      <Button 
+        onClick={handleNavigate} 
+        className="font-semibold cursor-pointer"
+      >
+        Welcome to Smilish
+      </Button>
+    </div>
+  );
+};
 
-export default page 
+export default Page;
+
+
+
+// import { Button } from "@/components/ui/button";
+// import React from "react";
+
+// const page = () => {
+//   return (
+//   <div>
+//     <Button>Baba and Mama</Button>
+//   </div>
+//   )
+// }
+
+// export default page 
