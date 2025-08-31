b2f1cbb21cbc3bef80c6e6069ba235d39c8a67e6
Chashmishlish <salishkhannn@gmail.com>
2025-08-31 23:42:14 +0500

    Suspense

--- components/Application/GlobalProvider.jsx
+++ components/Application/GlobalProvider.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import Loading from './loading'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const GlobalProvider = ({ children }) => {
   return (
@@ -14,8 +15,12 @@ const GlobalProvider = ({ children }) => {
             {children}
         </PersistGate>
       </Provider>
+      <Suspense fallback={null}>
+        <ReactQueryDevtools initialIsOpen={false}/> 
+        // keeping it false so that by default it shows icon not the content inside it
+      </Suspense>
     </QueryClientProvider>
-    
+
   )
 }
 
