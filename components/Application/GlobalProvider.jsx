852bbd8708e23138b0320a6f4ed98e0818474c7c
Chashmishlish <salishkhannn@gmail.com>
2025-08-31 23:44:17 +0500

    QueryClient

--- components/Application/GlobalProvider.jsx
+++ components/Application/GlobalProvider.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import Loading from './loading'
-import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 const GlobalProvider = ({ children }) => {
   return (
-    <QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <PersistGate persistor={persistor} loading={<Loading />}>
             {children}
