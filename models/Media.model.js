f490526fe70866d8e59dacaef5c23207f02472b8
Chashmishlish <salishkhannn@gmail.com>
2025-08-31 18:49:20 +0500

    Breadcrumb using shadcn.ui!!

---
+++ models/Media.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { GiTrumpetFlag } from "react-icons/gi";
+
+const mediaSchema = new mongoose.Schema({
+    asset_id:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    public_id:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    path:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    thumbnail_url:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    alt:{
+        type: String,
+    },
+    title:{
+        type: String,
+    },
+    deletedAt:{
+        type: Date,
+        default: null, 
+        index: GiTrumpetFlag
+    },
+    
+}, { timestamps: true })
+
+
+const MediaModel = mongoose.models.Media || mongoose.model('Media', mediaSchema, 'medias')
+export default OTPModel
