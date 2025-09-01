bfe7793d068abdc96011adc63cf2454e34793156
Chashmishlish <salishkhannn@gmail.com>
2025-09-02 02:13:00 +0500

    tO DELETE permanentlt!

--- app/api/media/delete/route.js
+++ app/api/media/delete/route.js
@@ -42,3 +42,35 @@ export async function PUT(request) {
         return catchError(error)
     }
 }
+
+export async function DELETE(request) {
+    try {
+        const auth = await isAuthenticated('admin')
+        if (!auth.isAuth) {
+            return response(false, 403, 'Unauthorized.')
+        }
+
+        await connectDB()
+        const payload = await request.json()
+
+        const ids = payload.ids || []
+        const deleteType = payload.deleteType
+
+        if (!Array.isArray(ids) || ids.length === 0) {
+            return response(false, 400, 'Invalid or empty ID list.')
+        }
+
+        const media = await MediaModel.find({ _id: { $in: ids } }).lean()
+        if (!media.length) {
+            return response(false, 404, 'Data not found.')
+        }
+
+        if (!deleteType === 'PD') {
+            return response(false, 400, 'Invalid delete operation. Delete type should be PD for this route. ')
+        }
+
+        await MediaModel.deleteMany({ _ids: { $in: ids } })
+    } catch (error) {
+        return catchError(error)
+    }
+}
