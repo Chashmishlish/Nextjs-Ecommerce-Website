aa566d5e6bd72bee7e2df571ffbba777b3d6e81a
Chashmishlish <salishkhannn@gmail.com>
2025-09-01 22:04:59 +0500

    trashing & all!

--- app/(root)/(admin)/admin/media/page.jsx
+++ app/(root)/(admin)/admin/media/page.jsx
@@ -1,30 +1,51 @@
 'use client'
 import BreadCrumb from '@/components/Application/Admin/BreadCrumb'
 import UploadMedia from '@/components/Application/Admin/UploadMedia'
-import { ADMIN_DASHBOARD } from '@/routes/AdminPanelRoutes'
-import React, {useState} from 'react'
-import { Card,  CardContent,  CardHeader} from '@/components/ui/card'
+import { ADMIN_DASHBOARD, ADMIN_MEDIA_SHOW } from '@/routes/AdminPanelRoutes'
+import React, { useEffect, useState } from 'react'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import Media from '@/components/Application/Admin/Media'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import { Label } from '@/components/ui/label'
+import { Checkbox } from '@/components/ui/checkbox'
 
 const breadcrumbData = [
-    {href: ADMIN_DASHBOARD, label: 'Home' },
-    {href: '', label: 'Media' },
+    { href: ADMIN_DASHBOARD, label: 'Home' },
+    { href: '', label: 'Media' },
 ]
 
 const MediaPage = () => {
 
     const [deleteType, setDeleteType] = useState('SD')  //bydefault SD
-    const [selectedMedia, setSelectedMedia] = useState([])  
+    const [selectedMedia, setSelectedMedia] = useState([])
+    const [selectAll, setSelectAll] = useState(false)
+
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        if (searchParams) {
+            const trashOf = searchParams.get('trashof')
+            setSelectedMedia([]);
+
+            if (trashOf) {
+                setDeleteType('PD');
+            } else {
+                setDeleteType('SD');
+            }
+        }
+    }, [searchParams])
 
     const fetchMedia = async (page, deleteType) => {
-        const {data: response} = await axios.get(`/api/media?page=${page}&&limit=10&&deleteType=${deleteType}`)
+        const { data: response } = await axios.get(`/api/media?page=${page}&&limit=10&&deleteType=${deleteType}`)
         console.log(response);
         return response
     };
 
-      const {
+    const {
         data,
         error,
         fetchNextPage,
@@ -33,65 +54,137 @@ const MediaPage = () => {
         isFetchingNextPage,
         status,
     } = useInfiniteQuery({
-         queryKey: ['media-data', deleteType],
-         queryFn: async ({ pageParam}) => await fetchMedia(pageParam, deleteType),
-         initialPageParam: 0,
-         getNextPageParam: (lastPage, pages) => {
-            const nextPage = pages.length 
-            return lastPage.hasMore ? nextPage : undefined 
-         },
-  })
-//   console.log(data);
-  
-const handleDelete = () => {
+        queryKey: ['media-data', deleteType],
+        queryFn: async ({ pageParam }) => await fetchMedia(pageParam, deleteType),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, pages) => {
+            const nextPage = pages.length
+            return lastPage.hasMore ? nextPage : undefined
+        },
+    })
+    //   console.log(data);
 
-}
+    const handleDelete = (selectedMedia, deleteType) => {
 
-  
-  return (
+    }
+
+    const handleSelectAll = () => {
+        setSelectAll(!selectAll)
+    }
+
+    useEffect(() => {
+        if (selectAll){
+            const ids = data.pages.flatMap(page => page.mediaData.map(media => media._id));
+        }
+    }, [selectAll] )
+
+    return (
         <div>
-            <BreadCrumb breadcrumbData={breadcrumbData}/>
+            <BreadCrumb breadcrumbData={breadcrumbData} />
             <Card className="py-0 rounded shadow-sm">
                 <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
                     <div className='flex justify-between items-center'>
-                        <h4 className='font semi-bold text-xl uppercase'>Media</h4>
+                        <h4 className='font-semibold text-xl uppercase'>
+
+                            {deleteType === 'SD' ? 'Media' : 'Media Trash'}
+
+                        </h4>
                         <div className='flex items-center gap-5'>
-                            <UploadMedia/>
+                            {deleteType === 'SD' && <UploadMedia />}
+
+
+                            <div className='flex gap-3'>
+                                {deleteType === 'SD' ? (
+                                    <Button type='button' variant='destructive'>
+                                        <Link href={`${ADMIN_MEDIA_SHOW}?trashof=media`}>
+
+                                            Trash
+                                        </Link>
+                                    </Button>
+                                ) : (
+                                    <Button type='button' >
+                                        <Link href={`${ADMIN_MEDIA_SHOW}`}>
+                                            Back To Media
+                                        </Link>
+                                    </Button>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </CardHeader>
                 <CardContent>
 
-    {
-        status === "pending"
-            ?
-            <div>Loading...</div>
-            :
-            status === 'error' ?
-            <div className='text-pink-500 text-sm'>
-                {error.message}
-            </div>
-            :
-            <div className='grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-2 mb-5'>
-                {
-                data?.pages?.map((page, index) => (
-                    <React.Fragment key={index}>
-                        {
-                            page?.mediaData?.map((media) => (
-                                <Media key={media._id}
-                                 media = {media}
-                                 handleDelete = {handleDelete}
-                                 deleteType = { deleteType }
-                                 selectedMedia = { selectedMedia }
-                                 setSelectedMedia = {setSelectedMedia}
+                    {selectedMedia.length > 0
+                        &&
+                        <div className='py-2 px-3 bg-pink-200 mb-2 rounded flex justify-between items-center'>
+                            <Label>
+                                <Checkbox
+                                    checked={selectAll}
+                                    onCheckedChange={handleSelectAll}
+                                    className="border-primary"
                                 />
-                            ))
-                        }
-                    </React.Fragment>
-                  ))
-                }
-            </div>
-    }
+                                Select All
+                            </Label>
+
+                            <div className='flex gap-2'>
+                                {deleteType === 'SD' ? (
+                                    <Button
+                                        variant='destructive'  className='cursor-pointer'
+                                        onClick={() => handleDelete(selectedMedia, deleteType)}
+                                    >
+                                        Move Into Trash
+                                    </Button>
+                                ) : (
+                                    <>
+                                        <Button
+                                            className='bg-green-500 hover:bg-green-600'
+                                            onClick={() => handleDelete(selectedMedia, "RSD")}
+                                        >
+                                            Restore
+                                        </Button>
+
+                                        <Button
+                                            variant='destructive'
+                                            onClick={() => handleDelete(selectedMedia, deleteType)}
+                                        >
+                                            Delete Permanently
+                                        </Button>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    }
+
+                    {
+                        status === "pending"
+                            ?
+                            <div>Loading...</div>
+                            :
+                            status === 'error' ?
+                                <div className='text-pink-500 text-sm'>
+                                    {error.message}
+                                </div>
+                                :
+                                <div className='grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-2 mb-5'>
+                                    {
+                                        data?.pages?.map((page, index) => (
+                                            <React.Fragment key={index}>
+                                                {
+                                                    page?.mediaData?.map((media) => (
+                                                        <Media key={media._id}
+                                                            media={media}
+                                                            handleDelete={handleDelete}
+                                                            deleteType={deleteType}
+                                                            selectedMedia={selectedMedia}
+                                                            setSelectedMedia={setSelectedMedia}
+                                                        />
+                                                    ))
+                                                }
+                                            </React.Fragment>
+                                        ))
+                                    }
+                                </div>
+                    }
                 </CardContent>
             </Card>
         </div>
