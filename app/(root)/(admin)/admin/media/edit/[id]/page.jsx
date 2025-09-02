e440c9a4971a65ba724aff57a694710a16d8b147
Chashmishlish <salishkhannn@gmail.com>
2025-09-02 17:52:00 +0500

    Issue form is not defined!!

--- app/(root)/(admin)/admin/media/edit/[id]/page.jsx
+++ app/(root)/(admin)/admin/media/edit/[id]/page.jsx
@@ -3,154 +3,86 @@ import { use } from 'react'
 import useFetch from '@/hooks/useFetch'
 import { ADMIN_DASHBOARD, ADMIN_MEDIA_SHOW } from '@/routes/AdminPanelRoutes'
 import BreadCrumb from '@/components/Application/Admin/BreadCrumb'
-import { Card , CardHeader } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import {  Form,  FormControl,  FormField,  FormItem,  FormLabel,  FormMessage,} from "@/components/ui/form";
+import React from "react";
+import { Input } from "@/components/ui/input";
+import ButtonLoading from "@/components/Application/ButtonLoading";
+
+
 
 const breadCrumbData = [
-  {
-    href: ADMIN_DASHBOARD,
-    label: 'Home'
-  },
-  {
-    href: ADMIN_MEDIA_SHOW,
-    label: 'Media'
-  },
-  {
-    href: "",
-    label: 'Edit Media'
-  },
+    {
+        href: ADMIN_DASHBOARD,
+        label: 'Home'
+    },
+    {
+        href: ADMIN_MEDIA_SHOW,
+        label: 'Media'
+    },
+    {
+        href: "",
+        label: 'Edit Media'
+    },
 
 ];
 
 const EditMedia = ({ params }) => {
-  const { id } = use(params);
-  const { data: mediaData } = useFetch(`/api/media/get/${id}`)
+    const { id } = use(params);
+    const { data: mediaData } = useFetch(`/api/media/get/${id}`)
 
-  // Prevent error by checking before render
-  // if (!mediaData) {
-  //   return <div>Loading...</div>;
-  // }
-  // console.log(mediaData);
+    // Prevent error by checking before render
+    // if (!mediaData) {
+    //   return <div>Loading...</div>;
+    // }
+    // console.log(mediaData);
 
-  return (
-    <div>
-      <BreadCrumb breadcrumbData={breadCrumbData} />
+    return (
+        <div>
+            <BreadCrumb breadcrumbData={breadCrumbData} />
 
-      <Card className="py-0 rounded shadow-sm">
-                      <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
-                          <div className='flex justify-between items-center'>
-                              <h4 className='font-semibold text-xl uppercase'>
-      
-                                  {deleteType === 'SD' ? 'Media' : 'Media Trash'}
-      
-                              </h4>
-                              <div className='flex items-center gap-5'>
-                                  {deleteType === 'SD' && <UploadMedia isMultiple={true} queryClient={queryClient} />}
-      
-      
-                                  <div className='flex gap-3'>
-                                      {deleteType === 'SD' ? (
-                                          <Button type='button' variant='destructive'>
-                                              <Link href={`${ADMIN_MEDIA_SHOW}?trashof=media`}>
-      
-                                                  Trash
-                                              </Link>
-                                          </Button>
-                                      ) : (
-                                          <Button type='button' >
-                                              <Link href={`${ADMIN_MEDIA_SHOW}`}>
-                                                  Back To Media
-                                              </Link>
-                                          </Button>
-                                      )}
-                                  </div>
-                              </div>
-                          </div>
-                      </CardHeader>
-                      <CardContent className='pb-5'>
-      
-                          {selectedMedia.length > 0
-                              &&
-                              <div className='py-2 px-3 bg-pink-200 mb-2 rounded flex justify-between items-center'>
-                                  <Label>
-                                      <Checkbox
-                                          checked={selectAll}
-                                          onCheckedChange={handleSelectAll}
-                                          className="border-primary"
-                                      />
-                                      Select All
-                                  </Label>
-      
-                                  <div className='flex gap-2'>
-                                      {deleteType === 'SD' ? (
-                                          <Button
-                                              variant='destructive' className='cursor-pointer'
-                                              onClick={() => handleDelete(selectedMedia, deleteType)}
-                                          >
-                                              Move Into Trash
-                                          </Button>
-                                      ) : (
-                                          <>
-                                              <Button
-                                                  className='bg-green-500 hover:bg-green-600'
-                                                  onClick={() => handleDelete(selectedMedia, "RSD")}
-                                              >
-                                                  Restore
-                                              </Button>
-      
-                                              <Button
-                                                  variant='destructive'
-                                                  onClick={() => handleDelete(selectedMedia, deleteType)}
-                                              >
-                                                  Delete Permanently
-                                              </Button>
-                                          </>
-                                      )}
-                                  </div>
-                              </div>
-                          }
-      
-                          {
-                              status === "pending"
-                                  ?
-                                  <div>Loading...</div>
-                                  :
-                                  status === 'error' ?
-                                      <div className='text-pink-500 text-sm'>
-                                          {error.message}
-                                      </div>
-                                      :
-                                      <>
-                                          {data.pages.flatMap(page => page.mediaData.map(media => media._id)).length === 0 && <div > Data not found. </div>}
-                                          <div className='grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-2 mb-5'>
-                                              {
-                                                  data?.pages?.map((page, index) => (
-                                                      <React.Fragment key={index}>
-                                                          {
-                                                              page?.mediaData?.map((media) => (
-                                                                  <Media key={media._id}
-                                                                      media={media}
-                                                                      handleDelete={handleDelete}
-                                                                      deleteType={deleteType}
-                                                                      selectedMedia={selectedMedia}
-                                                                      setSelectedMedia={setSelectedMedia}
-                                                                  />
-                                                              ))
-                                                          }
-                                                      </React.Fragment>
-                                                  ))
-                                              }
-                                          </div>
-                                      </>
-      
-                          }
-      
-                          {hasNextPage &&
-                          <ButtonLoading type="button" className="cursor-pointer" loading={isFetching} onClick={() => fetchNextPage()} text="Load More"/>
-                          }
-                      </CardContent>
-                  </Card>
-    </div>
-  );
+            <Card className="py-0 rounded shadow-sm">
+                <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
+                    <h4 className='text-xl font-semibold'>Edit Media </h4>
+                </CardHeader>
+                <CardContent className='pb-5'>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(handleLoginSubmit)}>
+                            <div className="mb-3">
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    type="email"
+                                                    placeholder="example@gmail.com"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+
+                            <div className="mb-3">
+                                <ButtonLoading
+                                    loading={loading}
+                                    type="submit"
+                                    text="Login"
+                                    className="w-full cursor-pointer"
+                                />
+                            </div>
+
+                        </form>
+                    </Form>
+                </CardContent>
+            </Card>
+        </div>
+    );
 };
 
 export default EditMedia
