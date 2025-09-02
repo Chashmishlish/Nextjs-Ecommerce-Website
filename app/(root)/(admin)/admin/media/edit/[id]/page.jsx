e89131c602a6078b204ee963f5de3ea4684148f5
Chashmishlish <salishkhannn@gmail.com>
2025-09-02 18:05:27 +0500

    resolved form error!

--- app/(root)/(admin)/admin/media/edit/[id]/page.jsx
+++ app/(root)/(admin)/admin/media/edit/[id]/page.jsx
@@ -1,14 +1,19 @@
 'use client'
 import { use } from 'react'
+import { useState } from "react"; // ADD THIS
 import useFetch from '@/hooks/useFetch'
 import { ADMIN_DASHBOARD, ADMIN_MEDIA_SHOW } from '@/routes/AdminPanelRoutes'
 import BreadCrumb from '@/components/Application/Admin/BreadCrumb'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import {  Form,  FormControl,  FormField,  FormItem,  FormLabel,  FormMessage,} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
 import React from "react";
 import { Input } from "@/components/ui/input";
 import ButtonLoading from "@/components/Application/ButtonLoading";
-
+import z from "zod";
+import { useForm } from 'react-hook-form'
+import { zSchema } from '@/lib/zodSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { showToast } from "@/lib/showToast";
 
 
 const breadCrumbData = [
@@ -36,6 +41,47 @@ const EditMedia = ({ params }) => {
     //   return <div>Loading...</div>;
     // }
     // console.log(mediaData);
+
+    const [loading, setLoading] = useState(false); // ADD THIS
+
+
+    const formSchema = zSchema.pick({
+        _id: true,
+        alt: true,
+        title: true
+    })
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    //   🔹 Login: check email + password, then send OTP
+    const handleLoginSubmit = async (values) => {
+        try {
+            setLoading(true);
+            const { data: loginResponse } = await axios.post("/api/auth/login", values);
+
+            if (!loginResponse.success) {
+                throw new Error(loginResponse.message);
+            }
+
+            setOtpEmail(values.email); // OTP email set karega
+            showToast("success", loginResponse.message || "OTP sent to your email.");
+
+        } catch (error) {
+            if (error.response?.status === 401 && error.response.data.message.includes("not verified")) {
+                showToast("info", error.response.data.message);
+            } else {
+                showToast("error", getErrorMessage(error));
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div>
