dab32f2706abb9951af34060f813d94057d3b029
Chashmishlish <salishkhannn@gmail.com>
2025-09-02 22:33:41 +0500

    Section: Add CategOry!!

--- app/(root)/(admin)/admin/category/add/page.jsx
+++ app/(root)/(admin)/admin/category/add/page.jsx
@@ -12,18 +12,16 @@ import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { useState } from 'react';
 import { useEffect } from "react";
 import slugify from 'slugify';
+import { showToast } from '@/lib/showToast';
+import axios from 'axios';
+const breadcrumbData = [
+  { href: ADMIN_DASHBOARD, label: 'Home' },
+  { href: ADMIN_CATEGORY_SHOW, label: 'Category' },
+  { href: "", label: 'Add Category' },
+];
 
-const addCategory = () => {
-
+const AddCategory = () => {
   const [loading, setLoading] = useState(false);
-
-  const breadcrumbData = [
-    { href: ADMIN_DASHBOARD, label: 'Home' },
-    { href: ADMIN_CATEGORY_SHOW, label: 'Category' },
-    { href: "", label: 'Add Category' },
-  ];
-
-
   const formSchema = zSchema.pick({
     name: true,
     slug: true
@@ -38,86 +36,98 @@ const addCategory = () => {
   });
 
   // console.log(form);
- useEffect (() => {
+  useEffect(() => {
     const name = form.getValues('name')
-    if(name){
+    if (name) {
       form.setValue('slug', slugify(name, { lower: true }));
       // form.setValue('slug', slugify(name).toLowerCase())
     }
   }, [form.watch('name')])
 
-const onSubmit = (values) => {
+  const onSubmit = async (values) => {
+    setLoading(true)
+    try {
+      const {data: response} = await axios.post('/api/category/create', values)
+    if(!response.success){
+      throw new Error(response.message)
+    }
+    form.reset()
+    showToast('success', response.message)
+    } catch (error) {
+      showToast('error', error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
 
+  return (
+    <div>
+      <BreadCrumb breadcrumbData={breadcrumbData} />
+
+      <Card className="py-0 rounded shadow-sm">
+        <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
+          <h4 className='text-xl font-semibold'> Add Category </h4>
+        </CardHeader>
+        <CardContent className='pb-5'>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+
+              <div className="mb-5">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          placeholder="Enter category name "
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="mb-5">
+                <FormField
+                  control={form.control}
+                  name="slug"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Slug</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          placeholder="Enter slug e.g. mobile-phones"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="mb-3">
+                <ButtonLoading
+                  loading={loading}
+                  type="submit"
+                  text=" Add Category"
+                  className="cursor-pointer"
+                />
+              </div>
+
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
 
-return (
-  <div>
-    <BreadCrumb breadcrumbData={breadcrumbData} />
-
-    <Card className="py-0 rounded shadow-sm">
-      <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
-        <h4 className='text-xl font-semibold'> Add Category </h4>
-      </CardHeader>
-      <CardContent className='pb-5'>
-
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-
-            <div className="mb-5">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="text"
-                        placeholder="Enter category name "
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="mb-5">
-              <FormField
-                control={form.control}
-                name="slug"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Slug</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="text"
-                        placeholder="Enter slug e.g. mobile-phones"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="mb-3">
-              <ButtonLoading
-                loading={loading}
-                type="submit"
-                text=" Add Category"
-                className="cursor-pointer"
-              />
-            </div>
-
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
-  </div>
-)
-}
-
-export default addCategory
+export default AddCategory
 //https://www.npmjs.com/package/slugify terminal: npm i slugify
