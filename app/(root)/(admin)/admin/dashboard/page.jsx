3664456a157dde22e5b4767be15fffa3e43c8040
Chashmishlish <salishkhannn@gmail.com>
2025-09-06 05:06:58 +0500

    Admin Dashboard in process!

--- app/(root)/(admin)/admin/dashboard/page.jsx
+++ app/(root)/(admin)/admin/dashboard/page.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
+import CountOverview from './CountOverview';
 
 const AdminDashboard = () => {
-  return (
-    <div className='font-bold text-4xl text-center mt-10'>
-      <h1 className='bg-gradient-to-r from-[#ef3a5d] via-pink-400 to-[#ef3a5d] bg-clip-text text-transparent drop-shadow-lg'>
-        Welcome to Smilish! ◡̈
-      </h1>
-    
-      <p className="text-gray-500 mt-2 text-lg">
-        By lish ✨
-      </p>
-    </div>
-  );
+ return(
+  <div className='pt-5'>
+    <CountOverview />
+  </div>
+ )
 };
 
 export default AdminDashboard;
