712818adb509d2a2520e2f6dffbfc0bd1b50deab
Shehzadi Alishba Khan <142626257+Chashmishlish@users.noreply.github.com>
2025-09-07 20:34:58 +0000

    Banner!!

--- app/(root)/(website)/page.jsx
+++ app/(root)/(website)/page.jsx
@@ -1,12 +1,40 @@
 import MainSlider from '@/components/Application/Website/MainSlider'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Banner1 from '@/public/assests/images/Slider1.png'
+import Banner2 from '@/public/assests/images/Slider4.png'
 
 const Home = () => {
   return (
-    <section>
-      <MainSlider/>
-    </section>
+    <>
+      <section>
+        <MainSlider />
+      </section>
+      <section className='lg:px-32 px-4 sm:pt-20 pt-5 pb-10'>
+        <div className='grid grid-cols-2 sm:gap-10 gap-2'>
+
+          <div className='border rounded-lg overflow-hidden'>
+            <Link href='' >
+             <Image src={Banner1.src} width={Banner1.width} height={Banner1.height} 
+             alt='Banner 1' 
+             className='transition-all hover:scale-110'
+             />
+            </Link>
+          </div>
+          <div className='border rounded-lg overflow-hidden'>
+            <Link href=''>
+             <Image src={Banner2.src} width={Banner2.width} height={Banner2.height} 
+             alt='Banner 2'
+             className='transition-all hover:scale-110'
+              />
+            </Link>
+          </div>
+
+        </div>
+      </section>
     
+    </>
   )
 }
 
