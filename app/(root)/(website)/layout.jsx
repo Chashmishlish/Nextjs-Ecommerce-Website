2c736123fe93e0ad955abc80caf70f4f955e9136
Chashmishlish <salishkhannn@gmail.com>
2025-09-07 14:39:05 +0500

    Basic setup for home!

--- app/(root)/(website)/layout.jsx
+++ app/(root)/(website)/layout.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Header from '@/components/Application/Website/Header'
+import Footer from '@/components/Application/Website/Footer'
+import { Kumbh_Sans } from 'next/font/google'
+
+const Kumbh = Kumbh_Sans({
+  weight: ['400', '500', '600', '700' , '800' , '900'],
+  display: 'swap' ,
+  subsets: ['latin']
+})
 
 const layout = ({ children }) => {
   return (
-    <div>
-      { children }
+    <div className={Kumbh.className}> 
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </div>
   )
 }
