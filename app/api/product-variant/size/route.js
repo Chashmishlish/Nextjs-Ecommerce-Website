48e39a3131995cc58f3a99687026ce4534d266f4
Chashmishlish <salishkhannn@gmail.com>
2025-09-12 00:49:57 +0500

    Map Result

--- app/api/product-variant/size/route.js
+++ app/api/product-variant/size/route.js
@@ -6,21 +6,22 @@ export async function GET() {
     try {
         await connectDB()
 
-const getSize = await ProductVariantModel.aggregate([
-            { $sort: {_id: 1}}, 
+        const getSize = await ProductVariantModel.aggregate([
+            { $sort: { _id: 1 } },
             {
                 $group: {
                     _id: "$size",
-                    first: {$first: "$_id"}
+                    first: { $first: "$_id" }
                 }
             },
-            {$sort: {first: 1}},
-            {$project: {_id: 0, size: "$_id"}}
+            { $sort: { first: 1 } },
+            { $project: { _id: 0, size: "$_id" } }
         ])
 
         if (!getSize.length) {
             return response(false, 404, 'Size not found.')
-         }
+        }
 
-        
-         
+        const sizes = getSize.map(item => item.size)
+
+        return response(true, 200, 'Size found.', sizes)
