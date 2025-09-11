c19dbdc1a5c96b7a34299074e733d1fe8f0aaa6e
Chashmishlish <salishkhannn@gmail.com>
2025-09-12 00:52:53 +0500

    Draft Version

--- app/api/product-variant/colors/route.js
+++ app/api/product-variant/colors/route.js
@@ -18,3 +18,24 @@ export async function GET() {
         return catchError(error)
     }
 }
+
+// // code check by gpt
+// import { connectDB } from "@/lib/databaseConnection"
+// import { catchError, response } from "@/lib/helperFunction"
+// import ProductVariantModel from "@/models/ProductVariant.model"
+
+// export async function GET() {
+//     try {
+//         await connectDB()
+
+//         const getColor = await ProductVariantModel.distinct("color")
+
+//         if (!getColor.length) {
+//             return response(false, 404, "Color not found.")
+//         }
+
+//         return response(true, 200, "Color found.", getColor)
+//     } catch (error) {
+//         return catchError(error)
+//     }
+// }
