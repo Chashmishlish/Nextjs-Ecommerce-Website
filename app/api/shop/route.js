1d76c63dc25106532dcc449ea6c9c373c7778fa3
Chashmishlish <salishkhannn@gmail.com>
2025-09-12 00:43:02 +0500

    QUERY PARAMS (Filters)

--- app/api/shop/route.js
+++ app/api/shop/route.js
@@ -9,4 +9,10 @@ export async function GET(request) {
         await connectDB()
         const searchParams = request.nextUrl.searchParams;
 
-     
+        // get filters from query params
+        const size = searchParams.get('size');
+        const color = searchParams.get('color');
+        const minPrice = parseInt(searchParams.get('minPrice')) || 0;
+        const maxPrice = parseInt(searchParams.get('maxPrice')) || 1000000;
+        const categorySlug = searchParams.get('category');
+        const search = searchParams.get('q')
