75564b1af7f787a95aac88df5188dbd9aa045b95
Chashmishlish <salishkhannn@gmail.com>
2025-09-21 01:02:15 +0500

    render OrderOverview , OrderStatus charts & LatestOrder, LatestReview tables

--- app/(root)/(admin)/admin/dashboard/page.jsx
+++ app/(root)/(admin)/admin/dashboard/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import CountOverview from './CountOverview';
 import QuickAdd from './QuickAdd';
@@ -8,22 +10,23 @@ import { OrderOverview } from './OrderOverview';
 import { OrderStatus } from './OrderStatus';
 import LatestOrder from './LatestOrder';
 import LatestReview from './LatestReview';
+import { ADMIN_ORDER_SHOW, ADMIN_REVIEW_SHOW } from '@/routes/AdminPanelRoutes';
 
 const AdminDashboard = () => {
- return(
-  <div className='pt-5'>
-    <CountOverview />
-    <QuickAdd />
-    
+  return (
+    <div className='pt-5'>
+      <CountOverview />
+      <QuickAdd />
 
-    <div className='mt-10 flex lg:flex-nowrap flex-wrap gap-10'>
+
+      <div className='mt-10 flex lg:flex-nowrap flex-wrap gap-10'>
         <Card className='rounded-lg lg:w-[70%] w-full p-0'>
           <CardHeader className=' py-3 border [.border=b]:pb-3'>
             <div className='flex justify-between items-center'>
               <span className='font-bold'>Order Overview</span>
               <Button type="button">
-                <Link href=''>View All</Link>
-                
+                <Link href={ADMIN_ORDER_SHOW}>View All</Link>
+
               </Button>
             </div>
           </CardHeader>
@@ -39,53 +42,52 @@ const AdminDashboard = () => {
             <div className='flex justify-between items-center'>
               <span className='font-bold'>Orders Status</span>
               <Button type="button">
-                <Link href=''>View All</Link>
-                
+                <Link href={ADMIN_ORDER_SHOW}>View All</Link>
+
               </Button>
             </div>
             <CardContent>
               <OrderStatus />
-          </CardContent>
+            </CardContent>
 
           </CardHeader>
         </Card>
-    </div>
-    <div className='mt-10 flex lg:flex-nowrap flex-wrap gap-10'>
+      </div>
+      <div className='mt-10 flex lg:flex-nowrap flex-wrap gap-10'>
         <Card className='rounded-lg lg:w-[70%] w-full p-0 block'>
           <CardHeader className=' py-3 border [.border=b]:pb-3'>
             <div className='flex justify-between items-center'>
               <span className='font-bold'> Latest Order </span>
               <Button type="button">
-                <Link href=''>View All</Link>
-                
+                <Link href={ADMIN_ORDER_SHOW}>View All</Link>
+
               </Button>
             </div>
           </CardHeader>
 
           <CardContent className='pt-3 lg:h-[350px] overflow-auto'>
-            <LatestOrder/>
+            <LatestOrder />
           </CardContent>
 
         </Card>
         <Card className='rounded-lg lg:w-[30%] w-full p-0 block'>
-          <CardHeader className=' py-3 border [.border=b]:pb-3'>
+          <CardHeader className='py-3 border-b pb-3'>
             <div className='flex justify-between items-center'>
               <span className='font-bold'>Latest Review</span>
               <Button type="button">
-                <Link href=''>View All</Link>
-                
+                <Link href={ADMIN_REVIEW_SHOW}>View All</Link>
               </Button>
             </div>
-            <CardContent className='pt-3 px-0 lg:h-[350px] overflow-auto'>
+          </CardHeader>
+          <CardContent className='pt-3 px-0 lg:h-[350px] overflow-auto'>
             <LatestReview />
           </CardContent>
-
-          </CardHeader>
         </Card>
+
+      </div>
+
     </div>
-         
-  </div>
- )
+  )
 };
 
 export default AdminDashboard;
