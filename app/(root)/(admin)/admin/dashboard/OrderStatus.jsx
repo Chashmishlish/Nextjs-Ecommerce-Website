1bcd157271d39b40b993dcc73fbbe1756abe5211
Chashmishlish <salishkhannn@gmail.com>
2025-09-21 00:58:24 +0500

    render PieChart with center label for total orders

--- app/(root)/(admin)/admin/dashboard/OrderStatus.jsx
+++ app/(root)/(admin)/admin/dashboard/OrderStatus.jsx
@@ -15,17 +15,19 @@ import {
     ChartTooltip,
     ChartTooltipContent,
 } from "@/components/ui/chart"
+import useFetch from "@/hooks/useFetch"
+import { useEffect, useState } from "react"
 
 export const description = "A donut chart"
 
-const chartData = [
-    { status: "pending", count: 275, fill: "var(--color-pending)" },
-    { status: "processing", count: 200, fill: "var(--color-processing)" },
-    { status: "shipped", count: 187, fill: "var(--color-shipped)" },
-    { status: "delivered", count: 173, fill: "var(--color-delivered)" },
-    { status: "cancel", count: 90, fill: "var(--color-cancel)" },
-    { status: "unverified", count: 90, fill: "var(--color-unverified)" },
-]
+// const chartData = [
+//     { status: "pending", count: 275, fill: "var(--color-pending)" },
+//     { status: "processing", count: 200, fill: "var(--color-processing)" },
+//     { status: "shipped", count: 187, fill: "var(--color-shipped)" },
+//     { status: "delivered", count: 173, fill: "var(--color-delivered)" },
+//     { status: "cancel", count: 90, fill: "var(--color-cancel)" },
+//     { status: "unverified", count: 90, fill: "var(--color-unverified)" },
+// ]
 
 const chartConfig = {
     status: {
@@ -59,6 +61,32 @@ const chartConfig = {
 }
 
 export function OrderStatus() {
+    const [chartData , setChartData] = useState([])
+    const [statusCount, setStatusCount] = useState()
+    const [totalCount, setTotalCount] = useState(0)
+    const {data: orderStatus, loading}= useFetch('/api/dashboard/admin/order-status')
+    console.log("Order Status",orderStatus)
+
+    useEffect(() => {
+  if(orderStatus && orderStatus.success){
+      const newOrderStatus = orderStatus.data.map((o) => ({
+          status: o._id,
+          count: o.count,
+          fill: `var(--color-${o._id})`
+      }));
+      setChartData(newOrderStatus);
+
+      const getTotalCount = orderStatus.data.reduce((acc, curr) => acc + curr.count, 0);
+      setTotalCount(getTotalCount);
+
+      const statusObj = orderStatus.data.reduce((acc, item) => {
+          acc[item._id] = item.count;
+          return acc;
+      }, {});
+      setStatusCount(statusObj);
+  }
+}, [orderStatus]);
+
     return (
         <div>
             <ChartContainer
@@ -69,7 +97,6 @@ export function OrderStatus() {
                     <ChartTooltip
                         cursor={false}
                         content={<ChartTooltipContent />}
-                    //   content={<ChartTooltipContent hideLabel />}
                     />
                     <Pie
                         data={chartData}
@@ -92,7 +119,7 @@ export function OrderStatus() {
                                                 y={viewBox.cy}
                                                 className="fill-foreground text-3xl font-bold"
                                             >
-                                                3006
+                                                {totalCount}
                                             </tspan>
                                             <tspan
                                                 x={viewBox.cx}
@@ -116,27 +143,27 @@ export function OrderStatus() {
             <ul>
                 <li className="flex justify-between items-center mb-3 text-sm">
                     <span>Pending</span>
-                    <span className='rounded-full px-2 text-sm bg-blue-500 text-white'>700</span>
+                    <span className='rounded-full px-2 text-sm bg-blue-500 text-white'>{statusCount?.pending || 0}</span>
                 </li>
                 <li className="flex justify-between items-center mb-3 text-sm">
                     <span>Processing</span>
-                    <span className='rounded-full px-2 text-sm bg-yellow-500 text-white'>500</span>
+                    <span className='rounded-full px-2 text-sm bg-yellow-500 text-white'>{statusCount?.processing || 0}</span>
                 </li>
                 <li className="flex justify-between items-center mb-3 text-sm">
                     <span>Shipped</span>
-                    <span className='rounded-full px-2 text-sm bg-purple-500 text-white'>450</span>
+                    <span className='rounded-full px-2 text-sm bg-purple-500 text-white'>{statusCount?.shipped || 0}</span>
                 </li>
                 <li className="flex justify-between items-center mb-3 text-sm">
                     <span>Delivered</span>
-                    <span className='rounded-full px-2 text-sm bg-green-500 text-white'>600</span>
+                    <span className='rounded-full px-2 text-sm bg-green-500 text-white'>{statusCount?.delivered || 0}</span>
                 </li>
                 <li className="flex justify-between items-center mb-3 text-sm">
                     <span>Cancel</span>
-                    <span className='rounded-full px-2 text-sm bg-cyan-500 text-white'>400</span>
+                    <span className='rounded-full px-2 text-sm bg-cyan-500 text-white'>{statusCount?.cancel || 0}</span>
                 </li>
                 <li className="flex justify-between items-center mb-3 text-sm">
                     <span>Unverified</span>
-                    <span className='rounded-full px-2 text-sm bg-orange-500 text-white'>356</span>
+                    <span className='rounded-full px-2 text-sm bg-orange-500 text-white'>{statusCount?.unverified || 0}</span>
                 </li>
             </ul>
 
