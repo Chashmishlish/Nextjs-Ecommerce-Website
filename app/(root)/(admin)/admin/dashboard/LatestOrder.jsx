f4dd351cb15ea310be0982fc18c4165b13c3b2fa
Chashmishlish <salishkhannn@gmail.com>
2025-09-21 00:55:54 +0500

    create LatestOrder component

--- app/(root)/(admin)/admin/dashboard/LatestOrder.jsx
+++ app/(root)/(admin)/admin/dashboard/LatestOrder.jsx
@@ -1,15 +1,39 @@
+'use client'
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import useFetch from "@/hooks/useFetch"
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import notFound from '@/public/assests/images/not-found.png'
+import { statusBadge } from "@/lib/helperFunction"
 
 const LatestOrder = () => {
+const [latestOrder, setLatestOrder] = useState([]);
+    const {data, loading} = useFetch('/api/dashboard/admin/latest-order')
 
+    useEffect(() => {
+        if(data && data.success){
+            setLatestOrder(data.data)
+        }
+    }, [data])
+
+    if(loading) return <div className="h-full w-ful flex justify-center items-center">Loading...</div>    
+    
+    if(!latestOrder || latestOrder.length === 0) return <div className="h-full w-ful flex justify-center items-center">
+        <Image 
+  src={notFound} 
+  height={80} 
+  width={80} 
+  alt="not found" 
+  className="w-20"
+/>
+    </div>
     return (
             <Table>
                 <TableHeader>
@@ -22,22 +46,16 @@ const LatestOrder = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {Array.from({ length: 30 }).map((_, i) => (
-                        <TableRow key={i}>
-                            <TableCell>{`SmiliSh-${i + 1}`}</TableCell>  
-                            <TableCell>{`SM-0 ${i + 1}`}</TableCell> 
-                            <TableCell>{`Item- ${i + 1}`}</TableCell> 
-                            <TableCell>{`Pending- ${i + 1}`}</TableCell> 
-                            <TableCell >{`300${i+1}`}</TableCell> 
+                    
+                    {latestOrder?.map((order) => (
+                        <TableRow key={order._id}>
+                            <TableCell>{order._id}</TableCell>  
+                            <TableCell>{order.paymentId || "N/A"}</TableCell> 
+                            <TableCell>{order.products.length}</TableCell> 
+                            <TableCell>{statusBadge(order.status)}</TableCell> 
+                            <TableCell>{order.totalAmount}</TableCell> 
                         </TableRow>
                     ))}
-                    {/* {Array.from({ length: 20 }).map((_, i) => {
-                        return (
-                            <TableRow key={i}>
-                                <TableCell>INV{i + 1}</TableCell>
-                            </TableRow>
-                        )
-                    })} */}
                 </TableBody>
             </Table>
     )
