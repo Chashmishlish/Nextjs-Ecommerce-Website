a636a0ed3205de71a9772c5d524b785a1a3e868e
Chashmishlish <salishkhannn@gmail.com>
2025-09-21 01:11:01 +0500

    SSR-Friendly Storage , RTK Query Integration

--- store/store.js
+++ store/store.js
@@ -3,18 +3,26 @@ import persistReducer from "redux-persist/es/persistReducer"
 import persistStore from "redux-persist/es/persistStore"
 import localStorage from "redux-persist/es/storage"
 // import storage from "redux-persist/lib/storage"
+import storage from "./persistStorage"; // 👈 fix for SSR
 import authReducer from "./reducer/authReducer"
 import cartReducer from "./reducer/cartReducer"
+import { authApi } from "./services/authApi";
 
 const rootReducer = combineReducers({
     authStore: authReducer,
-    cartStore: cartReducer
+    cartStore: cartReducer,
+      [authApi.reducerPath]: authApi.reducer,  // ✅ agar API slice use karna ho future me
+
 })
 
 
 const persistConfig = {
     key: 'root',
-    storage: localStorage
+    storage: localStorage,
+    whitelist: ["authStore", "cartStore"], // ✅ ab cart bhi persist hoga
+
+    //   whitelist: ["auth"], // ✅ only persist auth slice
+
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
@@ -22,7 +30,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 export const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({serializableCheck: false})
+        getDefaultMiddleware({
+            serializableCheck: false
+        })
+    .concat(authApi.middleware),  // ✅ agar RTK Query ka API use karna ho
+
 })
 
 export const persistor = persistStore(store)
