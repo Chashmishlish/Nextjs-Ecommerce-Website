e8504eac5b579923107b73c0e8fa1423d5a9155f
Chashmishlish <salishkhannn@gmail.com>
2025-09-21 01:58:20 +0500

    fetch latest 8 active products with media

--- app/api/product/get-featured-product/route.js
+++ app/api/product/get-featured-product/route.js
@@ -3,16 +3,16 @@ import { catchError, response } from "@/lib/helperFunction"
 import ProductModel from "@/models/Product.model"
 import MediaModel from "@/models/Media.model"
 
-export async function GET(){
+export async function GET() {
     try {
         await connectDB()
 
-        const getProduct = await ProductModel.find({deleteType: null}).populate('media').limit(8).lean()
+        const getProduct = await ProductModel.find({ deletedAt: null }).populate('media').limit(8).lean()
 
-        if(!getProduct){
-            return response (false, 404, 'Product not found.')
+        if (!getProduct) {
+            return response(false, 404, 'Product not found.')
         }
-        return response (true, 200, 'Product found.', getProduct)
+        return response(true, 200, 'Product found.', getProduct)
 
     } catch (error) {
         return catchError(error)
