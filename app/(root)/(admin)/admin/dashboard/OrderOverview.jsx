7148b71575d2c88b9b86418dffd5e95b035a9bfe
Chashmishlish <salishkhannn@gmail.com>
2025-09-21 00:57:27 +0500

    render BarChart with custom tooltip

--- app/(root)/(admin)/admin/dashboard/OrderOverview.jsx
+++ app/(root)/(admin)/admin/dashboard/OrderOverview.jsx
@@ -17,23 +17,40 @@ import {
     ChartTooltip,
     ChartTooltipContent,
 } from "@/components/ui/chart"
+import { useEffect, useState } from "react"
+import useFetch from "@/hooks/useFetch"
 
 export const description = "A bar chart"
 
-const chartData = [
-    { month: "January", amount: 200 },
-    { month: "February", amount: 305 },
-    { month: "March", amount: 237 },
-    { month: "April", amount: 73 },
-    { month: "May", amount: 209 },
-    { month: "June", amount: 214 },
-    { month: "July", amount: 674 },
-    { month: "August", amount: 555 },
-    { month: "September", amount: 450 },
-    { month: "October", amount: 34 },
-    { month: "November", amount: 234 },
-    { month: "December", amount: 100 },
-]
+// const chartData = [
+//     { month: "January", amount: 200 },
+//     { month: "February", amount: 305 },
+//     { month: "March", amount: 237 },
+//     { month: "April", amount: 73 },
+//     { month: "May", amount: 209 },
+//     { month: "June", amount: 214 },
+//     { month: "July", amount: 674 },
+//     { month: "August", amount: 555 },
+//     { month: "September", amount: 450 },
+//     { month: "October", amount: 34 },
+//     { month: "November", amount: 234 },
+//     { month: "December", amount: 100 },
+// ]
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
 const chartConfig = {
     amount: {
@@ -44,6 +61,24 @@ const chartConfig = {
 }
 
 export function OrderOverview() {
+    const [chartData, setChartData ] = useState([])
+    const {data: monthlySales, loading}= useFetch('/api/dashboard/admin/monthly-sales')
+    useEffect(() => {
+        if(monthlySales && monthlySales.success){
+            const getChartData = months.map((month, index) => {
+                const monthData = monthlySales.data.find(item => item._id.month === index + 1)
+                return {
+                    month: month,
+                    amount: monthData? monthData.totalSales : 0
+                }
+            })
+            setChartData(getChartData)
+            // console.log("MONTHLY SALES", monthlySales.data);  
+        }
+    }, [monthlySales])
+
+    console.log("DATA FROM CHART" , chartData);
+    
     return (
         <div>
             <ChartContainer config={chartConfig}>
